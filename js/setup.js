7b0412a66030ea7ec2a8f0f43d4f38088ed57dd1
TchernyavskyDaniil <tchernyavsky.daniil@yandex.ru>
2018-02-02 01:33:28 +0300

    need add style and fix some func

--- js/setup.js
+++ js/setup.js
@@ -21,11 +21,11 @@ var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content;
 
 var generateArray = function () {
-  var wizzardArr = [];
+  var wizardArr = [];
   for (var i = 0; i < MAGE_COUNT; i++) {
-    wizzardArr.push(generateWizzard());
+    wizardArr.push(generateWizzard());
   }
-  return wizzardArr;
+  return wizardArr;
 };
 
 var generateWizzard = function () {
@@ -36,21 +36,43 @@ var generateWizzard = function () {
   };
 };
 
+var drawWizzards = function (wizard) {
+  var wizardElement = similarWizardTemplate.cloneNode(true);
+
+  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
+  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+
+  return wizardElement;
+};
+
+var generateDocumentFragment = function (arr) {
+  var fragment = document.createDocumentFragment();
+
+  arr.forEach(function (wizardArr) {
+    var renderWizard = drawWizzards(wizardArr);
+    fragment.appendChild(renderWizard);
+  });
+  return fragment;
+};
+
 var getName = function () {
-  var randomName = getRandomNumber(MIN_RANGE, NAMES.length - 1);
-  var randomSurname = getRandomNumber(MIN_RANGE, SURNAMES.length - 1);
+  var randomName = getRandomNumber(MIN_RANGE, NAMES.length - 1).toFixed();
+  var randomSurname = getRandomNumber(MIN_RANGE, SURNAMES.length - 1).toFixed();
   return NAMES[randomName] + ' ' + SURNAMES[randomSurname];
 };
 
 var getCoatColor = function () {
-  var randomCoat = getRandomNumber(MIN_RANGE, COAT_COLORS.length -1 );
+  var randomCoat = getRandomNumber(MIN_RANGE, COAT_COLORS.length - 1).toFixed();
   return COAT_COLORS[randomCoat];
 };
 
 var getEyesColor = function () {
-  var randomEyes = getRandomNumber(MIN_RANGE, EYES_COLORS.length - 1);
+  var randomEyes = getRandomNumber(MIN_RANGE, EYES_COLORS.length - 1).toFixed();
   return EYES_COLORS[randomEyes];
 };
 var getRandomNumber = function (min, max) {
   return Math.random() * (max - min) + min;
 };
+
+similarListElement.appendChild(generateDocumentFragment(generateArray()));
